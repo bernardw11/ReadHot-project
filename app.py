7cc6acc10f76334dde0c9faaa4351b3a45eef39e
allyyim <alisonyim3@gmail.com>
2020-07-24 21:38:50 +0000

    finished library page demo

--- app.py
+++ app.py
@@ -3,18 +3,17 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from datetime import datetime
 # from flask_pymongo import PyMongo
 
 
-# -- Initialization section --
+# # -- Initialization section --
 app = Flask(__name__)
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+# events = [
+#         {"event":"First Day of Classes", "date":"2019-08-21"},
+#         {"event":"Winter Break", "date":"2019-12-20"},
+#         {"event":"Finals Begin", "date":"2019-12-01"}
+#     ]
 
 # name of database
 # app.config['MONGO_DBNAME'] = 'database-name'
@@ -31,8 +30,7 @@ events = [
 @app.route('/index')
 
 def index():
-    return render_template('index.html', events = events, time = datetime.now())
-
+    return render_template('library_index.html')
 
 # CONNECT TO DB, ADD DATA
 
