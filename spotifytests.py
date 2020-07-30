a8c4adae9c043ed6c1e42d612814d3b391ada3a4
bernardw11 <bwang00@stuy.edu>
2020-07-30 00:06:42 -0400

    okokokokokokok back up and running PLEASE

--- spotifytests.py
+++ spotifytests.py
@@ -7,15 +7,14 @@ from ibm_watson import ToneAnalyzerV3
 from ibm_watson.tone_analyzer_v3 import ToneInput
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-client_id = '0617e5b2a9c540349ea50ca2e3547e8a'
-client_secret = '2f254374e3db4c4b9f7f0f38f5cc7554'
-redirect_uri = 'http://127.0.0.1:5000/callback/q'
-scope = "user-library-read playlist-modify"
+client_id = '7c22514bc25d4b2483f5471d9d1b6dc9'
+client_secret = '394d743f7c2c4cb591aed988038b5bab'
+redirect_uri = 'http://localhost:8888/callback'
+scope = "playlist-modify"
 
-sp2 = spotipy.Spotify(auth_manager = SpotifyOAuth(scope = scope,
-    client_id = client_id, client_secret = client_secret, redirect_uri = redirect_uri, username = "test_username"))
-user_id = sp2.current_user()['id']
-
+sp3 = spotipy.Spotify(auth_manager = SpotifyOAuth(scope = scope,
+    client_id = client_id, client_secret = client_secret, redirect_uri = redirect_uri, username = "bruh"))
+user_id = sp3.current_user()['id']
 
 # Authentication via IAM
 authenticator = IAMAuthenticator('GKKyb6SAR11iYpZjZPy9Bnhu3SIC0Nmjc7befjGW3GfV')
@@ -41,7 +40,7 @@ def get_tempo(item):
 def get_feature(item, feature):
     if item['track']:
         id = item['track']['id']
-        feature = sp2.audio_features([id])[0][feature]
+        feature = sp3.audio_features([id])[0][feature]
         return feature
     return 0
 
@@ -72,7 +71,7 @@ def descending_list(tracklist, feature):
             track = item['track']
             print(f"{idx} {track['artists'][0]['name']} - {track['name']}")
             id = track['id']
-            feature_level = sp2.audio_features([id])[0][feature]
+            feature_level = sp3.audio_features([id])[0][feature]
             print(f"{feature} level: {feature_level}")
 def ascending_list(tracklist, feature):
     #SAME DEAL WITH THIS FUNCTION!!! LITERALLY ALL WE REMOVE IS reverse = True
@@ -92,19 +91,19 @@ def ascending_list(tracklist, feature):
         track = item['track']
         print(f"{idx} {track['artists'][0]['name']} - {track['name']}")
         id = track['id']
-        feature_level = sp2.audio_features([id])[0][feature]
+        feature_level = sp3.audio_features([id])[0][feature]
         print(f"{feature} level: {feature_level}")
 
 #you can make this have a ascending/descending parameter! and then change it so
 #ascending_list or descending_list is called, or just make a function called
 #sort_items or sort_list that does the whole shebang.
 def order_playlist(playlist_name, feature):
-    playlists = sp2.current_user_playlists()
+    playlists = sp3.current_user_playlists()
     for playlist in playlists['items']:
         name = playlist['name']
         if name == playlist_name:
             id = playlist['id']
-            tracks = sp2.playlist_tracks(id)
+            tracks = sp3.playlist_tracks(id)
             descending_list(tracks, feature)
 '''
 The below code is kinda nifty! allows you to go thru any playlist and
@@ -117,12 +116,12 @@ print out the songs in a sorted order.
 
 #goes through your entire library to find songs that fit the parameters
 def compile_songs_from_library(feature, sign, level):
-    all_music = sp2.current_user_saved_tracks(limit = 50)
+    all_music = sp3.current_user_saved_tracks(limit = 50)
     total_number = all_music['total']
     list_of_ids = []
     counter = 0
     while counter < total_number and len(list_of_ids) < 100:
-        all_music = sp2.current_user_saved_tracks(limit = 50, offset = counter)
+        all_music = sp3.current_user_saved_tracks(limit = 50, offset = counter)
         for idx, item in enumerate(all_music['items']):
             counter += 1
             track = item['track']
@@ -142,10 +141,10 @@ def compile_songs_from_library(feature, sign, level):
 #create_playlist(name, feature, >/<. 0-1.0)
 def create_playlist(new_playlist_name, feature, sign, level):
     #creates playlist and stores it as new_playlist_id
-    new_playlist_id = sp2.user_playlist_create(user_id, new_playlist_name, description = "Created by ReadHot")['id']
+    new_playlist_id = sp3.user_playlist_create(user_id, new_playlist_name, description = "Created by ReadHot")['id']
     list_of_ids = compile_songs_from_library(feature, sign, level)
     #adds a song
-    sp2.user_playlist_add_tracks(user_id, new_playlist_id, list_of_ids)
+    sp3.user_playlist_add_tracks(user_id, new_playlist_id, list_of_ids)
     return new_playlist_id
     print(f"added new playlist: {new_playlist_name}!")
 '''the below code searches thru ur library and creates a playlist!'''
@@ -158,7 +157,7 @@ def create_playlist(new_playlist_name, feature, sign, level):
 
 def get_songs_from_playlist(features, playlist_id, difficulty):
     viable_songs = []
-    tracklist = sp2.playlist_tracks(playlist_id)
+    tracklist = sp3.playlist_tracks(playlist_id)
     for idx, item in enumerate(tracklist['items']):
         if item['track']:
             track = item['track']
@@ -166,8 +165,8 @@ def get_songs_from_playlist(features, playlist_id, difficulty):
             id = track['id']
             viable = True
             for feature, level in features.items():
-                feature_value = sp2.audio_features([id])[0][feature]
-                deviation = len(features) * 0.1 * (2 / difficulty)
+                feature_value = sp3.audio_features([id])[0][feature]
+                deviation = len(features) * 0.1 * (4 / difficulty)
                 if feature_value < (level - deviation) or feature_value > (level + deviation):
                     viable = False
                 # else:
@@ -241,12 +240,12 @@ def generate_playlist(title, author, description, subjects, displayname):
 
     '''Songs from album is DONE DONE DONE DONE DONE'''
     #gets songs from the official album on spotify
-    data = sp2.search(q = title, type = "album")
+    data = sp3.search(q = title, type = "album")
     albums = data["albums"]
     if albums["items"]:
         albumid = albums["items"][0]["id"]
         print(albumid)
-        albumdata = sp2.album_tracks(albumid)
+        albumdata = sp3.album_tracks(albumid)
         for track in albumdata['items']:
             list_of_song_ids.append(track['id'])
 
@@ -269,7 +268,8 @@ def generate_playlist(title, author, description, subjects, displayname):
         for subject in subjects:
             if genre in subject or genre == subject:
                 for id in knownplaylists[genre]:
-                    playlistids.append(id)
+                    if id not in playlistids:
+                        playlistids.append(id)
     # for subject in subjects:
     #     if subject in knownplaylists:
     #         for id in knownplaylists[subject]:
@@ -296,6 +296,6 @@ def generate_playlist(title, author, description, subjects, displayname):
         list_of_song_ids = list_of_song_ids[:30]
 
 
-    new_playlist_id = sp2.user_playlist_create(user_id, f"{title} by {author}", description = f"For {displayname} - Created by ReadHot")['id']
-    sp2.user_playlist_add_tracks(user_id, new_playlist_id, list_of_song_ids)
+    new_playlist_id = sp3.user_playlist_create(user_id, f"{title} by {author}", description = f"For {displayname} - Created by ReadHot")['id']
+    sp3.user_playlist_add_tracks(user_id, new_playlist_id, list_of_song_ids)
     return new_playlist_id
